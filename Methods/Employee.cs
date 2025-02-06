7f500407428a09996c232caade94a30b37eb0111
inessSaa <inesssaada809@gmail.com>
2025-02-06 09:43:18 +0100

    009 Methods

---
+++ Methods/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class Employee
+    {
+
+        public static double TAX = 0.03;
+
+        public string FName;
+        public string LName;
+        public double Wage;
+        public double LoggedHours;
+
+        // Refactor salary slip application
+
+        private double Calculate() => Wage * LoggedHours;
+        private double CalculateTax() => Calculate() * TAX;
+        private double CalculateNet() => Calculate() - CalculateTax();
+
+        public string PrintSlip()
+        {
+            return
+            $"\nFirst Name :{FName}" +
+            $"\nLast Name :{LName}" +
+            $"\nWage :{Wage}" +
+            $"\nLogged Hours :{LoggedHours}" +
+            $"\n --------------------" +
+            $"\nSalary :{Calculate()}" +
+            $"\nDeductable Tax {Employee.TAX * 100}% Amount :${CalculateTax()}" +
+
+            $"\nNet Salary :{CalculateNet()}\n";
+
+        }
+    }
+}
+
